2bf725af4040f5774879008040d63171fe14d36b
xaviergodbout <48611188+xaviergodbout@users.noreply.github.com>
2025-06-10 18:08:39 -0400

    added folders to the config

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/js");
     eleventyConfig.addPassthroughCopy("src/img");
+
+    eleventyConfig.addPassthroughCopy("src/downloads");
+    eleventyConfig.addPassthroughCopy("src/projects");
     return {
         dir: {
             input: "src", // Source files
